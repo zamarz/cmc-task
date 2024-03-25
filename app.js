b52bcec01da8c9c08b1275925b5493779794d6e3
zamarz <zachmarzouk@gmail.com>
2024-03-25 13:50:10 +0000

    Tweak CSS file

--- app.js
+++ app.js
@@ -1,46 +1,13 @@
 function addItemToList() {
-  let newEntryForList = document.getElementById("listEntry").value.trim();
+  let newEntryForList = document.getElementById("formEntry").value.trim();
   if (newEntryForList) {
-    // data.push(newEntryForList);
     confirm("Your item has been successfully added");
   } else {
     alert("Please enter a task before you submit.");
   }
 
-  document.getElementById("listEntry").value = "";
+  document.getElementById("formEntry").value = "";
 }
-
-// let data = ["Read a book", "Water plants", "Take a walk"];
-
-// addNewListItem();
-
-// function addNewListItem() {
-//   const listTasks = document.getElementById("listTasks");
-
-//   let ul = `<ul id="tasksToStyle">${data
-//     .map(
-//       (item) =>
-//         `<li>${item}<button type="button" id="deleteButton">Delete</button></li>`
-//     )
-//     .join("")}
-//         </ul>`;
-//   listTasks.innerHTML = ul;
-// }
-
-// const itemAdder = document.getElementById("itemAdder");
-// itemAdder.addEventListener("click", addNewListItem);
-
-// let list = document.querySelector("ul");
-
-// list.addEventListener(
-//   "click",
-//   function (event) {
-//     if (event.target.tagName === "LI") {
-//       event.target.classList.toggle("checked");
-//     }
-//   },
-//   false
-// );
 
 const listItems = document.querySelectorAll("ul li");
 
